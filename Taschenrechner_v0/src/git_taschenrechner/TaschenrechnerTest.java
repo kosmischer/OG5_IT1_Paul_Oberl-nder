c7cd83887c55b71d86b32c95bd7e0c3837187085
Paul <pauleoberlaender123@gmail.com>
2026-09-03 10:13:19 +0200

    A2.5 Taschenrechner vervollständigt

--- Taschenrechner_v0/src/git_taschenrechner/TaschenrechnerTest.java
+++ Taschenrechner_v0/src/git_taschenrechner/TaschenrechnerTest.java
@@ -8,8 +8,10 @@ public class TaschenrechnerTest {
 		// TODO Auto-generated method stub
 
 		Scanner myScanner = new Scanner(System.in);
+		
 		Taschenrechner ts = new Taschenrechner();
 
+		
 		int swValue;
 
 		// Display menu graphics
@@ -20,21 +22,32 @@ public class TaschenrechnerTest {
 		System.out.println("|        1. Addieren       |");
 		System.out.println("|        2. Subtrahieren   |");
 		System.out.println("|        3. Dividieren     |");
-		System.out.println("|        4. Exit           |");
+		System.out.println("|        4. Dividieren     |");
+		System.out.println("|        5. Exit           |");
 		System.out.println("============================");
 		System.out.print(" Select option: ");
 		swValue = myScanner.next().charAt(0);
-
-		// Switch construct
+		
+		System.out.println("Please enter the two Numbers (make sure to seperate them with enter)");
+		
+		double n1 = myScanner.nextDouble();
+		double n2 = myScanner.nextDouble();
+		
+	
 		switch (swValue) {
 		case '1':
-			System.out.println("4 + 5 = " + ts.add(4, 5));
+			System.out.println( n1 + " + " + n2 + " = " + ts.add(n1, n2));
+		case '2':
+			System.out.println( n1 + " - " + n2 + " = " + ts.sub(n1, n2));
+		case '3':
+			System.out.println( n1 + " x " + n2 + " = " + ts.mul(n1, n2));
 			break;
-			
-			
-		  //  add your code here
-		  
-		  
+		case '4':
+			System.out.println( n1 + " : " + n2 + " = " + ts.div(n1, n2));
+			break;
+		case '5':
+			System.out.println("Bye!");
+			break;
 		default:
 			System.out.println("Invalid selection");
 			break; // This break is not really necessary
